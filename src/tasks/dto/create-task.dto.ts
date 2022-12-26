04f32f0dc2c1d0a23e97f5feef1a266ed8005f40
Pedro Basilio <artiumdominus@protonmail.com>
2022-12-26 12:08:43 -0300

    validate task status to update

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,7 +1,6 @@
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateTaskDto {
-
   @IsNotEmpty()
   title: string;
 
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -37,7 +38,11 @@ export class TasksController {
   }
 
   @Patch(':id/status')
-  updateTaskStatus(@Param('id') id, @Body('status') status): Task {
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  ): Task {
+    const { status } = updateTaskStatusDto;
     return this.tasksService.updateTaskStatus(id, status);
   }
 
